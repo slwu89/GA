3e0e11b129b1da216325e0ee769e52da893e19ff
slwu89 <slwu89@berkeley.edu>
2017-12-06 19:29:34 -0800

    add tournament selection

--- selection.R
+++ selection.R
@@ -14,14 +14,14 @@
 #' Khalid Jebari, Mohammed Madiafi (2013) Selection Methods for Genetic Algorithms. https://www.researchgate.net/publication/259461147_Selection_Methods_for_Genetic_Algorithms. 
 #' @export
 #' 
-
-
 selection <- function(pop_fitness,fitness,P,P_ix){
   if(fitness == "weight"){
     # sort by objective function (minimum of AIC)
     minimum=min(pop_fitness)
-    relative = lapply(pop_fitness, function(x) exp(-0.01*(x-minimum))) #minimum?
-    weights  = lapply(relative, function(x) x / sum(unlist(relative)))
+    relative = vapply(X = pop_fitness,FUN = function(x){exp(-0.01*(x-minimum))},FUN.VALUE = numeric(1),USE.NAMES = FALSE)
+    # relative = lapply(pop_fitness, function(x) exp(-0.01*(x-minimum))) #minimum?
+    weights = vapply(X = relative,FUN = function(x){x/sum(relative)},FUN.VALUE = numeric(1))
+    # weights  = lapply(relative, function(x) x / sum(unlist(relative)))
     # selection
     selection_ix = sample(x = P_ix, size = P, replace = TRUE, prob = weights)
   } 
@@ -33,5 +33,48 @@ selection <- function(pop_fitness,fitness,P,P_ix){
     selection_ix = sample(x = P_ix, size = P, replace = TRUE, prob = pop_rank_final)
   }
   
+  return(selection_ix)
+}
+
+
+#' Tournament Selection Of Models for Recombination
+#' Selection is used to determine parent chromosomes fitness for recombination in the genetic algorithm for model optimization. Fitness is assessed based on AIC or an inputted fitness function. AIC is used in two ways: one through rank and another through weight. Weight takes the absolute fitness to determine probability of recombination while rank uses relative ranking of fitness of models. This fitness assessment determines likelihood to 'reproduce' or carry on genetic makeup into the t+1 generation. 
+#' 
+#' selection(pop_fitness,fitness,P,P_ix)
+#'
+#' 
+#' @param pop_fitness a vector indicating fitness of each chrosomosome in the current generation based on the evaluated fitness function. 
+#' @param fitness a character indicating whether to use rank or weight when comparing model fitness.
+#' @param P Population size, corresponding to the number of genes or covariates on each chromosome or model considered for recombination. 
+#' @param P_ix a sequence of integers, ending value P. 
+#' @param k size of disjoin subset (must be smaller than P and whose quotient with P is 0, eg; P mod k = 0)
+#' 
+#' Author(s):
+#' References: 
+#' Khalid Jebari, Mohammed Madiafi (2013) Selection Methods for Genetic Algorithms. https://www.researchgate.net/publication/259461147_Selection_Methods_for_Genetic_Algorithms. 
+#' @export
+#' 
+selection_tournament <- function(pop_fitness,fitness,P,P_ix,k){
+  if(P %% k != 0 | k >= P){stop("tournament selection parameter k must be less than P and have P mod k = 0")}
+  if(fitness == "weight"){
+    # sort by objective function (minimum of AIC)
+    minimum=min(pop_fitness)
+    relative = lapply(pop_fitness, function(x) exp(-0.01*(x-minimum))) #minimum?
+    weights  = lapply(relative, function(x) x / sum(unlist(relative)))
+  } else {
+    # sort by rank (given by formula: 2*ri / P(P+1))
+    pop_rank = rank(-pop_fitness)
+    weights = (2*pop_rank) / (P*(P+1))
+  }
+  # implement tournament
+  selection_ix = rep(NaN,P)
+  while(any(is.nan(selection_ix))){
+    random_partition = split(x = P_ix,f = sample(x = P_ix,size = k,replace = FALSE))
+    champions = vapply(X = random_partition,FUN = function(x){
+      x[order(pop_fitness[x],decreasing = TRUE)][1]
+    },FUN.VALUE = integer(1),USE.NAMES = FALSE)
+    i = which(is.nan(selection_ix))[1]
+    selection_ix[i:(i+k-1)] = champions
+  }
   return(selection_ix)
 }
